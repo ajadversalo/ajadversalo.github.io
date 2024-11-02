ba862e53a4d34ff8397ad532d60fadd5c226a42c
ajadversalo <ajadvers@gmail.com>
2024-11-02 14:59:44 -0700

    add close resume

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Resume() {
         
     return(
-        <div className={"h-[90vh] overflow-y-scroll"}>                        
+        <div className={"h-[90vh] overflow-y-scroll mt-8"}>                        
             <div className="">                
                 <img  src={'resume1.jpg'} style={{width: '100%'}} alt='resume1'/>
                 <img  src={'resume2.jpg'} style={{width: '100%'}} alt='resume2'/>
