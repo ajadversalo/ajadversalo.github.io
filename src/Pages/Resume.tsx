f77ea58cae52473f302e27380f76d0dd4be1f711
ajadversalo <ajadvers@gmail.com>
2025-07-09 16:33:33 -0700

    More resume updates

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -6,7 +6,9 @@ function Resume() {
         <div className={"h-[90vh] overflow-y-scroll mt-8"}>                        
             <div className="">                
                 <img  src={'resume1.jpg'} style={{width: '100%'}} alt='resume1'/>
-                <img  src={'resume2.jpg'} style={{width: '100%'}} alt='resume2'/>
+                <div className="mt-[-6rem]">
+                  <img src={'resume2.jpg'} style={{ width: '100%' }} alt='resume2' />
+                </div>
             </div>          
         </div>
     );
